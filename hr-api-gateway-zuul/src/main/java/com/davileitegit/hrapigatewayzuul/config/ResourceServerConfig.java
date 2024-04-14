ed9fb11608ea4c0bf11bea8d6c5e778ccd75a4e2
Davi <deussecreadger@gmail.com>
2024-04-14 18:51:11 -0300

    Fix cors configuration

--- hr-api-gateway-zuul/src/main/java/com/davileitegit/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/davileitegit/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -54,18 +54,17 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		corsConfig.setAllowedOrigins(Arrays.asList("*"));
 		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
 		corsConfig.setAllowCredentials(true);
-		corsConfig.setAllowedHeaders(Arrays.asList("Authorizayion", "Content-Type"));
-		
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfig);
-		
 		return source;
-	} 
-	
-	@Bean 
-	public FilterRegistrationBean<CorsFilter> corsFilter(){
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
 		FilterRegistrationBean<CorsFilter> bean 
-		= new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+			= new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
 		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return bean;
 	}
