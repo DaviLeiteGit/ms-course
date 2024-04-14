f4742b65d16a32fe2090966280c28abb3e10fb72
Davi <deussecreadger@gmail.com>
2024-04-14 11:21:16 -0300

    Configuring hr-config-server project

--- hr-worker/src/main/java/com/davileitegit/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/davileitegit/hrworker/resources/WorkerResource.java
@@ -36,6 +36,13 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
+		try {
+			Thread.sleep(300L);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		Worker worker = repository.findById(id).get();
