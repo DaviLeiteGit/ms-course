174f558d9416d728710662bbdbb350f4224763c6
Davi <deussecreadger@gmail.com>
2024-04-14 19:49:11 -0300

    db config server

--- hr-worker/src/main/java/com/davileitegit/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/davileitegit/hrworker/resources/WorkerResource.java
@@ -48,13 +48,13 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
-		
-		try {
-			Thread.sleep(300L);
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-		}
+//		
+//		try {
+//			Thread.sleep(300L);
+//		} catch (Exception e) {
+//			
+//			e.printStackTrace();
+//		}
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
